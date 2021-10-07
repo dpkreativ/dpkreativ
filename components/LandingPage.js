c944d935850250a36f570e1f4b7efc4f1ee291a7
Divine Orji <dpkreativ@gmail.com>
2021-10-07 10:58:46 +0100

    tweaking about section

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -40,9 +40,17 @@ const LandingPage = () => {
               <h3 className="font-bold text-4xl mb-5">
                 Hi 👋, I'm Divine Orji.
               </h3>
-              <p>
+              <p className="text-xl lg:text-2xl leading-relaxed my-5">
                 I have over 2 years of experience building user-focused
                 interfaces.
+              </p>
+              <p className="text-xl lg:text-2xl leading-relaxed my-5">
+                Currently I'm working as a technical writer at Hackmamba.
+              </p>
+              <p className="text-xl lg:text-2xl leading-relaxed my-5">
+                Before now, I was a software engineer at Truparse, where I
+                collaborated with other members of the team to build interfaces
+                for outsourced client projects.
               </p>
             </div>
           </div>
