d2ef4f9558937298330e9ae88640342894521682
Divine Orji <dpkreativ@gmail.com>
2021-12-27 07:05:09 +0100

    add shadow to header

--- components/General/Header.js
+++ components/General/Header.js
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 bg-white">
+    <header className="sticky top-0 z-50 bg-white shadow-sm">
       <Nav />
     </header>
   );
