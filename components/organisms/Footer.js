a0cc6dbcf7a37168c73bc55c72479e870e252367
Divine Orji <dpkreativ@gmail.com>
2021-12-27 16:36:53 +0100

    layout footer

--- components/organisms/Footer.js
+++ components/organisms/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Copyright from "../atoms/Copyright";
+import Social from "../molecules/Social";
 
 const Footer = () => {
-  return <footer>Made in Nigeria</footer>;
+  return (
+    <footer className="bg-purple-600 text-white p-5 lg:p-10">
+      <section className="flex justify-between border-t border-white py-10">
+        <Copyright />
+        <Social />
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
