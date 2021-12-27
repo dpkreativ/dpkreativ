78758cb19a9b2f12d1c70a61e74e2b32e0338645
Divine Orji <dpkreativ@gmail.com>
2021-12-27 16:36:42 +0100

    import social media icons

---
+++ components/molecules/Social.js
@@ -0,0 +1,13 @@
+import { GitHub, LinkedIn, Twitter } from "../atoms/Icons";
+
+const Social = () => {
+  return (
+    <div className="flex space-x-3">
+      <GitHub />
+      <LinkedIn />
+      <Twitter />
+    </div>
+  );
+};
+
+export default Social;
