ed3e11fc492b7f1e0a47fac739e68a703fca38da
Divine Orji <dpkreativ@gmail.com>
2021-12-28 00:26:01 +0100

    create hero component

---
+++ components/organisms/Hero.js
@@ -0,0 +1,39 @@
+import Button from "../atoms/Button";
+import ProfileImage from "../molecules/ProfileImage";
+
+const Hero = () => {
+  return (
+    <section className="grid md:grid-cols-2">
+      <div className="px-6 py-12 flex flex-col items-center order-last md:order-first md:items-end justify-center">
+        <h2 className="text-2xl lg:text-4xl text-center md:text-right md:w-4/5 mb-10 leading-normal">
+          <span className="bg-purple-600 text-white">Passionate</span>
+          <span> about building </span>
+          <span className="italic font-semibold">fast</span>,
+          <span className="italic font-semibold"> scalable</span>
+          <span> web apps, while creating </span>
+          <span className="italic font-semibold">beautiful</span>
+          <span> user interfaces.</span>
+        </h2>
+        <div className="flex space-x-3 w-max">
+          <div>
+            <Button onClick={() => (location.href = "/resume")}>Résumé</Button>
+          </div>
+          <div>
+            <Button onClick={() => (location.href = "/projects")} filled>
+              Projects
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative h-96">
+        <div className="bg-purple-600 h-full w-full absolute md:w-4/5 md:right-0"></div>
+        <div className="w-60 h-72 md:w-64 md:h-80 md:absolute z-10 top-1/2 -translate-y-1/2">
+          <ProfileImage />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
