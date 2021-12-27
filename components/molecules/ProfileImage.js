71713a1f1f4cee9a1f8fad982a7d20f7bcfa3b8a
Divine Orji <dpkreativ@gmail.com>
2021-12-28 00:50:53 +0100

    ui tweaks and bug fixes

--- components/molecules/ProfileImage.js
+++ components/molecules/ProfileImage.js
@@ -3,12 +3,9 @@ import CustomImage from "../atoms/CustomImage";
 const ProfileImage = () => {
   return (
     <div className="relative">
-      <div className="w-48 h-56 md:w-60 md:h-72 border border-white absolute top-8 left-8"></div>
-      <div className="w-48 h-56 md:w-60 md:h-72 bg-purple-300 absolute z-10 top-4 left-4 shadow-xl">
-        <CustomImage
-          url="/assets/divi.jpg"
-          className="w-48 h-56 md:w-60 md:h-72"
-        />
+      <div className="w-60 h-72 border border-white absolute top-6 left-6"></div>
+      <div className="w-60 h-72 bg-purple-300 absolute z-10 top-2 left-1 shadow-xl">
+        <CustomImage url="/assets/divi.jpg" className="w-60 h-72" />
       </div>
     </div>
   );
