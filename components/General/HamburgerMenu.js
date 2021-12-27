a278aff17fdf52b83ec8b3d79c7f54f85e18de0d
Divine Orji <dpkreativ@gmail.com>
2021-12-27 07:04:59 +0100

    add width and height as props

--- components/General/HamburgerMenu.js
+++ components/General/HamburgerMenu.js
@@ -1,4 +1,4 @@
-const HamburgerMenu = ({ onClick, className }) => {
+const HamburgerMenu = ({ onClick, className, width = "40", height = "40" }) => {
   return (
     <>
       <button
@@ -6,7 +6,7 @@ const HamburgerMenu = ({ onClick, className }) => {
         id="hamburgerMenu"
         onClick={onClick}
       >
-        <svg width="50" height="50" viewBox="0 0 100 100">
+        <svg width={width} height={height} viewBox="0 0 100 100">
           <path
             className="line line1"
             d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
