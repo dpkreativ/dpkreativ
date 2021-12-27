f4a0dab6f3213b817e3e41c3f4fe761842671af2
Divine Orji <dpkreativ@gmail.com>
2021-12-27 13:18:02 +0100

    create blog page

---
+++ pages/blog/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import PageTemplate from "../../components/templates/PageTemplate";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Page Head */}
+      <Head>
+        <title>Divine Orji - Blog</title>
+        <meta name="description" content="Divine Orji's portfolio website" />
+        <link rel="icon" href="/assets/kreativ.ico" />
+      </Head>
+
+      {/* Page Body */}
+      <PageTemplate activeIndex={5}>
+        <section>Hi, this is the blog</section>
+      </PageTemplate>
+    </div>
+  );
+}
