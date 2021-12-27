cb26ebc41253781950ad2f6db58e1bd2fac01e4a
Divine Orji <dpkreativ@gmail.com>
2021-12-28 00:26:28 +0100

    update landing page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import Button from "../components/atoms/Button";
+import Hero from "../components/organisms/Hero";
 import PageTemplate from "../components/templates/PageTemplate";
 
 export default function Home() {
@@ -16,51 +15,7 @@ export default function Home() {
       {/* Page Body */}
       <PageTemplate activeIndex={1}>
         {/* Hero Section */}
-        <section className="h-screen">
-          <div className="grid lg:grid-cols-2 gap-12 h-5/6">
-            <div className="py-12 flex flex-col items-end justify-center">
-              <h2 className="text-4xl text-right lg:w-4/5 mb-10 leading-normal">
-                <span className="bg-purple-600 text-white">Passionate</span>
-                <span> about building </span>
-                <span className="italic font-semibold">fast</span>,
-                <span className="italic font-semibold"> scalable</span>
-                <span> web apps, while creating </span>
-                <span className="italic font-semibold">beautiful</span>
-                <span> user interfaces.</span>
-              </h2>
-              <div className="flex space-x-3 w-max">
-                <div>
-                  <Button onClick={() => (location.href = "/resume")}>
-                    Résumé
-                  </Button>
-                </div>
-                <div>
-                  <Button onClick={() => (location.href = "/projects")} filled>
-                    Projects
-                  </Button>
-                </div>
-              </div>
-            </div>
-
-            <div className="relative">
-              <div className="bg-purple-600 w-4/5 h-full absolute right-0"></div>
-              <div className="w-64 h-80 absolute z-10 top-1/2 -translate-y-1/2">
-                <div className="relative">
-                  <div className="w-64 h-72 border absolute top-4 border-white"></div>
-                  <div className="w-60 h-72 bg-purple-300 absolute z-10 top-0 shadow-xl">
-                    <div className="relative h-72 w-60">
-                      <Image
-                        src="/assets/divi.jpg"
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Hero />
 
         {/* About Me */}
         {/* <section>
