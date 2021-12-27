1ae28024efb0f424a046d32e5bdd1caec3983221
Divine Orji <dpkreativ@gmail.com>
2021-12-27 16:36:01 +0100

    copyright component created

---
+++ components/atoms/Copyright.js
@@ -0,0 +1,9 @@
+const Copyright = () => {
+  return (
+    <div className="font-semibold">
+      &copy; Divine Orji, <span>{new Date().getFullYear()}</span>
+    </div>
+  );
+};
+
+export default Copyright;
