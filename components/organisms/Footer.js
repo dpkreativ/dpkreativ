01e76daf8716d39b127d312bb4c8ae12dbeadf36
Divine Orji <dpkreativ@gmail.com>
2021-12-28 00:25:31 +0100

    create footer component

--- components/organisms/Footer.js
+++ components/organisms/Footer.js
@@ -1,10 +1,12 @@
-import React from "react";
 import Copyright from "../atoms/Copyright";
 import Social from "../molecules/Social";
 
 const Footer = () => {
   return (
-    <footer className="bg-purple-600 text-white p-5 lg:p-10">
+    <footer
+      className="bg-purple-600 text-white p-5 lg:p-10 flex-grow-0 flex-shrink"
+      style={{ flexBasis: 40 }}
+    >
       <section className="flex justify-between border-t border-white py-10">
         <Copyright />
         <Social />
