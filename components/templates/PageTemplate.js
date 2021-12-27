4a866e452eb9b0832acc21b15b8ca5d968b6e363
Divine Orji <dpkreativ@gmail.com>
2021-12-28 00:26:12 +0100

    update page template

--- components/templates/PageTemplate.js
+++ components/templates/PageTemplate.js
@@ -3,16 +3,21 @@ import Header from "../organisms/Header";
 
 const PageTemplate = ({ activeIndex, children }) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen h-full justify-between">
       {/* Header Section */}
       <Header active={activeIndex} />
 
       {/* Main Content */}
-      <main className="min-h-screen">{children}</main>
+      <main
+        className="flex-grow flex-shrink h-full"
+        style={{ flexBasis: "auto" }}
+      >
+        {children}
+      </main>
 
       {/* Footer Section */}
       <Footer />
-    </>
+    </div>
   );
 };
 
