fac3f4db86cc101497f59814ff246eb19758fc29
Divine Orji <dpkreativ@gmail.com>
2021-12-28 01:19:37 +0100

    add page title to contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import PageTitle from "../../components/molecules/PageTitle";
 import PageTemplate from "../../components/templates/PageTemplate";
 
 export default function Home() {
@@ -13,7 +14,9 @@ export default function Home() {
 
       {/* Page Body */}
       <PageTemplate activeIndex={5}>
-        <section>Hi, this is the contact page</section>
+        <section>
+          <PageTitle title="Contact" />
+        </section>
       </PageTemplate>
     </div>
   );
