2fe02ef03d6a15219eccfc3cb21b0081f96359c6
Divine Orji <dpkreativ@gmail.com>
2021-12-28 01:19:16 +0100

    optimize page title UI component

--- components/molecules/PageTitle.js
+++ components/molecules/PageTitle.js
@@ -1,11 +1,9 @@
 const PageTitle = ({ title }) => {
   return (
-    <div className="pt-12 md:pt-16">
-      <div className="p-5 md:p-8 bg-purple-600 w-3/5">
-        <h1 className="font-semibold text-right text-white text-4xl md:text-5xl">
-          {title}
-        </h1>
-      </div>
+    <div className="p-5 md:p-8 bg-purple-600 w-3/5">
+      <h1 className="font-semibold text-right text-white text-3xl md:text-5xl">
+        {title}
+      </h1>
     </div>
   );
 };
