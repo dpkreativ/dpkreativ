2aa8b070178116ef496e270386edb1ff7c36d601
Divine Orji <dpkreativ@gmail.com>
2021-12-28 01:19:59 +0100

    add page title to resume page

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import PageTitle from "../../components/molecules/PageTitle";
 import PageTemplate from "../../components/templates/PageTemplate";
 
 export default function Home() {
@@ -13,7 +14,9 @@ export default function Home() {
 
       {/* Page Body */}
       <PageTemplate activeIndex={2}>
-        <section>Hi, this is the resume page</section>
+        <section>
+          <PageTitle title="Résumé" />
+        </section>
       </PageTemplate>
     </div>
   );
