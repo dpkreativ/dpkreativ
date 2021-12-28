ab4eb3fbd3a4b8dec1663287c1165e7a3b6c3f7e
Divine Orji <dpkreativ@gmail.com>
2021-12-28 01:27:44 +0100

    bug and security fixes

--- components/atoms/CustomImage.js
+++ components/atoms/CustomImage.js
@@ -3,7 +3,12 @@ import Image from "next/image";
 const CustomImage = ({ url, className }) => {
   return (
     <div className={`relative ${className}`}>
-      <Image src={url} layout="fill" objectFit="cover" />
+      <Image
+        src={url}
+        layout="fill"
+        objectFit="cover"
+        alt="Divine's profile picture"
+      />
     </div>
   );
 };
--- components/molecules/Social.js
+++ components/molecules/Social.js
@@ -3,13 +3,17 @@ import { GitHub, LinkedIn, Twitter } from "../atoms/Icons";
 const Social = () => {
   return (
     <div className="flex space-x-3">
-      <a href="https://github.com/dpkreativ" target="_blank">
+      <a href="https://github.com/dpkreativ" target="_blank" rel="noreferrer">
         <GitHub />
       </a>
-      <a href="https://www.linkedin.com/in/dpkreativ" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/dpkreativ"
+        target="_blank"
+        rel="noreferrer"
+      >
         <LinkedIn />
       </a>
-      <a href="https://twitter.com/dpkreativ" target="_blank">
+      <a href="https://twitter.com/dpkreativ" target="_blank" rel="noreferrer">
         <Twitter />
       </a>
     </div>
