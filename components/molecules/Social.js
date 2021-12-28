d522f14fafd7efee462f4b2ab0fd16d1797bb003
Divine Orji <dpkreativ@gmail.com>
2021-12-28 01:25:02 +0100

    add links to social media

--- components/molecules/Social.js
+++ components/molecules/Social.js
@@ -3,9 +3,15 @@ import { GitHub, LinkedIn, Twitter } from "../atoms/Icons";
 const Social = () => {
   return (
     <div className="flex space-x-3">
-      <GitHub />
-      <LinkedIn />
-      <Twitter />
+      <a href="https://github.com/dpkreativ" target="_blank">
+        <GitHub />
+      </a>
+      <a href="https://www.linkedin.com/in/dpkreativ" target="_blank">
+        <LinkedIn />
+      </a>
+      <a href="https://twitter.com/dpkreativ" target="_blank">
+        <Twitter />
+      </a>
     </div>
   );
 };
