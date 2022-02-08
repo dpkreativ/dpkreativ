a4f393f0657132562ec3ca3d046962926961287f
Divine <dpkreativ@gmail.com>
2022-02-08 04:46:28 +0100

    update resume

--- components/organisms/Resume.js
+++ components/organisms/Resume.js
@@ -23,7 +23,8 @@ const Resume = () => {
       <section className="mb-24">
         <SectionTitle title={<span className="font-semibold">Skills</span>} />
         <div className="lg:text-2xl flex flex-col space-y-4 md:space-y-8 py-6 max-w-5xl mx-auto">
-          JavaScript, React, MongoDB
+          CSS, JavaScript, ReactJS, NextJS, Nuxtjs, Sass, TailwindCSS,
+          TypeScript. Technical writing and documentation.
         </div>
       </section>
 
@@ -32,13 +33,54 @@ const Resume = () => {
         <SectionTitle
           title={<span className="font-semibold">Experience</span>}
         />
-        <div className="lg:text-2xl flex flex-col space-y-4 md:space-y-8 py-6 max-w-5xl mx-auto">
+        <div className="lg:text-2xl flex flex-col space-y-5 md:space-y-8 py-6 max-w-5xl mx-auto">
           <Experience
-            title="Software Engineer"
+            title="Contributing Technical Content Writer and Frontend Engineer"
+            org="Hackmamba"
+            duration="Jan 2021 - Present"
+          >
+            <div className="grid grid-cols-12 gap-x-3">
+              <div className="col-span-1 text-right">-</div>
+              <div className="col-span-11">
+                Create technical content on building products with Cloudinary
+                and NextJS/Nuxtjs.
+              </div>
+              <div className="col-span-1 text-right">-</div>
+              <div className="col-span-11">
+                Organize hackathons and review community submissions.
+              </div>
+            </div>
+          </Experience>
+          <Experience
+            title="Software Engineer (Frontend)"
             org="Truparse"
             duration="Feb 2021 - July 2021"
           >
-            Hello Amigo
+            <div className="grid grid-cols-12 gap-x-3">
+              <div className="col-span-1 text-right">-</div>
+              <div className="col-span-11">
+                Handled regular tasks on software development and collaborated
+                with other developers to create apps and functionalities that
+                improve user experience.
+              </div>
+            </div>
+          </Experience>
+          <Experience
+            title="Software Engineer (Frontend)"
+            org="Payvest Global"
+            duration="Oct 2020 - Jan 2021"
+          >
+            <div className="grid grid-cols-12 gap-x-3">
+              <div className="col-span-1 text-right">-</div>
+              <div className="col-span-11">
+                Structured, wrote and maintained multiple project codebases.
+              </div>
+              <div className="col-span-1 text-right">-</div>
+              <div className="col-span-11">
+                Converted whiteboard ideas, solutions from wireframes, and final
+                designs into code.
+              </div>
+            </div>
           </Experience>
         </div>
       </section>
