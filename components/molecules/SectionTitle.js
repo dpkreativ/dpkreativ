9dd2fe44c80ca6fcf79a208e48f900454e5aa897
Divine <dpkreativ@gmail.com>
2022-02-08 04:21:19 +0100

    still testing animations

--- components/molecules/SectionTitle.js
+++ components/molecules/SectionTitle.js
@@ -1,6 +1,10 @@
 const SectionTitle = ({ title }) => {
   return (
-    <div className="w-max">
+    <div
+      className="w-max"
+      data-aos="fade-up"
+      data-aos-anchor-placement="center-bottom"
+    >
       <h1 className="text-2xl lg:text-4xl">{title}</h1>
       <div className="bg-primary-normal w-4/5 h-4 -mt-2"></div>
     </div>
