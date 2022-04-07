875df20b0a10a376b81761aea5524aeec77d8f2a
Divine <dpkreativ@gmail.com>
2022-04-07 16:40:52 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         primary: {
-          normal: "#03499A",
-          dark: "#03438C",
-          light: "#87BEFD",
+          normal: '#03499A',
+          dark: '#03438C',
+          light: '#87BEFD',
         },
         secondary: {},
         accent: {},
