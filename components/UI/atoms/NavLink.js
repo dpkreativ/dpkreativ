fedaabb2a0c58d1bb22b9b52114b6ff8da0e2878
Divine Orji <dpkreativ@gmail.com>
2022-05-29 19:31:44 +0100

    Add bottom-border class

--- components/UI/atoms/NavLink.js
+++ components/UI/atoms/NavLink.js
@@ -1,9 +1,16 @@
-const NavLink = ({ children, className, rightBorder, leftBorder, active }) => {
+const NavLink = ({
+  children,
+  className,
+  rightBorder,
+  leftBorder,
+  bottomBorder,
+  active,
+}) => {
   return (
     <div
       className={`text-secondary-01 border-line relative ${
         rightBorder && 'border-r'
-      } ${leftBorder && 'border-l'}`}
+      } ${leftBorder && 'border-l'} ${bottomBorder && 'border-b'}`}
     >
       <div className="p-5">{children}</div>
       <div
