b0e68afefea5e889d57e2582d2702d023dfcef52
Divine Orji <dpkreativ@gmail.com>
2022-05-29 19:52:53 +0100

    Display or hide modal

--- components/UI/molecules/MobileNavbar.js
+++ components/UI/molecules/MobileNavbar.js
@@ -1,16 +1,25 @@
-import { HamburgerIcon } from '../atoms/Icons';
+import { useState } from 'react';
+import { CloseIcon, HamburgerIcon } from '../atoms/Icons';
 import Modal from '../atoms/Modal';
 
 const MobileNavbar = ({ children }) => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="md:hidden">
       {/* Hamburger Menu */}
-      <section className="p-5 text-secondary-01">
-        <HamburgerIcon />
+      <section
+        className="p-5 text-secondary-01"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        {showMenu ? <CloseIcon /> : <HamburgerIcon />}
       </section>
-      <section>
-        <Modal className="top-16 text-secondary-01 md:hidden">{children}</Modal>
-      </section>
+      {showMenu ? (
+        <section>
+          <Modal className="top-16 text-secondary-01 md:hidden">
+            {children}
+          </Modal>
+        </section>
+      ) : null}
     </nav>
   );
 };
