c03d2103933b6bddeeaf56aafc5eef9371bd6a73
Divine Orji <dpkreativ@gmail.com>
2022-05-29 20:12:08 +0100

    Add social links to Footer for mobile

--- components/UI/organisms/Footer.js
+++ components/UI/organisms/Footer.js
@@ -4,7 +4,7 @@ import Navbar from '../molecules/Navbar';
 
 const Footer = () => {
   return (
-    <footer className="border-t border-line text-secondary-01 flex">
+    <footer className="border-t border-line text-secondary-01 flex justify-between">
       <div className="p-5 w-full max-w-max md:border-r border-line">
         find me on:
       </div>
@@ -23,6 +23,21 @@ const Footer = () => {
           </NavLink>
         </div>
       </Navbar>
+
+      {/* Mobile Navlinks. Might refactor later */}
+      <section className="md:hidden">
+        <div className="flex">
+          <NavLink leftBorder>
+            <Twitter />
+          </NavLink>
+          <NavLink leftBorder>
+            <LinkedIn />
+          </NavLink>
+          <NavLink leftBorder>
+            <GitHub />
+          </NavLink>
+        </div>
+      </section>
     </footer>
   );
 };
