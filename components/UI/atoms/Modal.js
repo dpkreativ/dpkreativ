2e070a58dff8b66f21849cb367a671096e363efe
Divine Orji <dpkreativ@gmail.com>
2022-05-29 19:52:04 +0100

    Make modal more generic so it can be easily reused

--- components/UI/atoms/Modal.js
+++ components/UI/atoms/Modal.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Modal = ({ children, className, show = true }) => {
+const Modal = ({ children, className }) => {
   const [isBrowser, setIsBrowser] = useState(false);
 
   useEffect(() => {
     setIsBrowser(true);
   }, []);
 
-  // Modal Content
-  const modalContent = show ? (
+  // === MODAL CONTENT ===
+  // Set up the basic layout of the modal
+  const modalContent = (
     <div className={`absolute w-full max-w-lg ${className}`}>{children}</div>
-  ) : null;
+  );
 
+  // === DISPLAY MODAL ===
+  // If the browser has finished loading and useEffect has run, create a modal component in the #modal-root node.
   if (isBrowser) {
     return ReactDOM.createPortal(
       modalContent,
