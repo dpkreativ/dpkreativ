64bf354a8cf2f26982824519ffdaa7dcbcd3e7db
Divine Orji <dpkreativ@gmail.com>
2022-05-29 20:53:27 +0100

    Add content for the homepage

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,36 @@ export default function Home() {
 
       {/* Page Body */}
       <PageTemplate>
-        {/* <div className="text-primary-04">Hello Divine. Let's begin!</div>
-        <Button>start-game</Button> */}
+        <section className="p-5 my-20 md:my-32 mx-auto w-full max-w-5xl grid gap-5 md:grid-cols-2">
+          <article>
+            <div className="text-primary-04">
+              <h3 className="text-lg leading-loose">Hi there! I'm</h3>
+              <h1 className="text-6xl">Divine Orji</h1>
+              <p className="text-accent-02 md:text-secondary-03 text-xl md:text-3xl mt-2">
+                &#62; Sofware Engineer && Technical Writer
+              </p>
+            </div>
+            {/* <Button>start-game</Button> */}
+            <div className="text-sm mt-20">
+              <p className="text-secondary-01">
+                &#47;&#47; find my profile on GitHub
+              </p>
+              <p>
+                <span className="text-secondary-03">const</span>
+                <span className="text-accent-02"> githubLink</span>
+                <span> = </span>
+                <span className="text-accent-03">
+                  <a href="https://github.com/dpkreativ" target="_blank">
+                    "https://github.com/dpkreativ"
+                  </a>
+                </span>
+              </p>
+            </div>
+          </article>
+          <article className="hidden md:block">
+            {/* Put a sample project here */}
+          </article>
+        </section>
       </PageTemplate>
     </div>
   );
