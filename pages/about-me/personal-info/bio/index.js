a250efe08654d42ef26f4404d5edfd2956264a2d
Divine Orji <dpkreativ@gmail.com>
2022-06-26 03:07:43 +0100

    fix bugs

--- pages/about-me/personal-info/bio/index.js
+++ pages/about-me/personal-info/bio/index.js
@@ -21,12 +21,14 @@ export default function AboutMe() {
           <section className="grid grid-cols-2 h-full w-full">
             <article className="lg:border-r border-line h-full py-4 px-8 text-secondary-01 flex space-x-5">
               <div className="text-right">
-                {displayNumbers(10).map((el) => (
-                  <div>{el}</div>
+                {displayNumbers(10).map((el, idx) => (
+                  <div key={idx}>
+                    {el}, {idx}
+                  </div>
                 ))}
               </div>
               <div>
-                <p>/*</p>
+                <p>{`/*`}</p>
                 <p>ABOUT ME:</p>
                 <p>
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -34,7 +36,7 @@ export default function AboutMe() {
                   ratione commodi, eaque aspernatur assumenda! Deleniti beatae
                   sint obcaecati velit fugit consequuntur quae ducimus ea.
                 </p>
-                <p>*/</p>
+                <p>{`/*`}</p>
               </div>
             </article>
             <article></article>
