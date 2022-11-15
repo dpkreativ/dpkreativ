a4aafdd0394df1472b9df085d79a87cda7c55b6f
Divine Orji <dpkreativ@gmail.com>
2022-11-15 09:48:41 +0100

    Test out @next/font and it works

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { Dancing_Script, Inter } from '@next/font/google';
+import '../styles/globals.css';
+
+const inter = Inter();
+const dancingScript = Dancing_Script();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${inter.style.fontFamily};
+          }
+          .dance {
+            font-family: ${dancingScript.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
