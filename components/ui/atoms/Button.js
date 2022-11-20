fe3347e7f7253d538645a743b0259e7f5f777e99
Divine Orji <dpkreativ@gmail.com>
2022-11-20 16:36:15 +0100

    Mobile footer created

--- components/ui/atoms/Button.js
+++ components/ui/atoms/Button.js
@@ -1,6 +1,6 @@
 export default function Button({ children, type }) {
   return (
-    <button type={type} className="bg-black text-white rounded-lg p-5">
+    <button type={type} className="bg-black text-white rounded-lg p-4">
       {children}
     </button>
   );
--- components/ui/organisms/Footer.js
+++ components/ui/organisms/Footer.js
@@ -1,7 +1,43 @@
+import {
+  GithubIcon,
+  HashnodeIcon,
+  HeartIcon,
+  LinkedInIcon,
+} from '../../assets/Icons';
+import { LogoName } from '../../assets/Logos';
+
 export default function Footer() {
   return (
-    <footer>
-      <div>dpkreativ@gmail.com</div>
+    <footer className="bg-black text-white p-4 grid gap-24">
+      <div className="flex flex-col space-y-6 items-center">
+        <div className="text-2xl">dpkreativ@gmail.com</div>
+        <div className="flex items-center space-x-4">
+          <div>
+            <HashnodeIcon />
+          </div>
+          <div>
+            <GithubIcon />
+          </div>
+          <div>
+            <LinkedInIcon />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-6 items-center">
+        <div className="text-2xl dance flex space-x-2 items-center">
+          <div>made with</div>
+          <div>
+            <HeartIcon />
+          </div>
+          <div>by dpkreativ</div>
+        </div>
+        <div className="flex space-x-2 text-2xl items-center">
+          <div>
+            <LogoName />
+          </div>
+          <div>&copy; {new Date().getFullYear()}</div>
+        </div>
+      </div>
     </footer>
   );
 }
