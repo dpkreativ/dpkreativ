2a62191a984cfc40b7e3db3e6b0f80752b45b66e
Divine Orji <dpkreativ@gmail.com>
2023-08-26 03:41:44 +0100

    set up landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/page.module.css";
+import Link from "next/link";
+import { GitHub, LinkedIn, Twitter } from "@/components/icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,84 +10,98 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <a
-          href="https://github.com/dpkreativ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>
-            <code className={styles.code}>GitHub</code>
-          </p>
-        </a>
-        <div>
-          <a
-            href="https://linkedin.com/in/dpkreativ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered By{" "}
+        <div className={styles.top}>
+          <Link href="/">
             <Image
               src="/Logo-02.svg"
               alt="Kreativ Logo"
-              className={styles.vercelLogo}
+              className="logo"
               width={100}
               height={24}
               priority
             />
-          </a>
+          </Link>
         </div>
+        <nav>
+          <div className="flex gap-5">
+            <a
+              href="https://github.com/dpkreativ"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-icon"
+            >
+              <GitHub />
+            </a>
+            <a
+              href="https://linkedin.com/in/dpkreativ"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-icon"
+            >
+              <LinkedIn />
+            </a>
+            <a
+              href="https://twitter.com/dpkreativ"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-icon"
+            >
+              <Twitter />
+            </a>
+          </div>
+        </nav>
       </div>
 
       <div className={styles.center}>
-        <h2 className={inter.className}>
-          <div>Divine Orji</div>
-          <div>software engineer</div>
-          <div>technical writer</div>
-        </h2>
+        <div>
+          <h2 className={`text-5xl font-semibold ${inter.className}`}>Hi!</h2>
+
+          <p className={`mt-10 ${styles.code}`}>
+            I'm Divine. I began coding in 2019, and since then I've been
+            exploring different technologies to make helpful software.
+          </p>
+          <p className={`my-10 ${styles.code}`}>
+            I love creating attractive, user-friendly interfaces, and
+            documenting my experiences with different aspects of software
+            engineering.
+          </p>
+        </div>
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="/blog"
+        <Link
+          href="/projects"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={inter.className}>
-            Blog <span>-&gt;</span>
+            My projects <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>
-            A collection of all my technical articles. Check them out.
-          </p>
-        </a>
+          <p className={inter.className}>Cool stuff I've worked on.</p>
+        </Link>
 
-        <a
-          href="https://twitter.com/dpkreativ"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link href="/blog" className={styles.card}>
           <h2 className={inter.className}>
-            Twitter <span>-&gt;</span>
+            My blog <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Shitposts, memes, and the occasional geeky stuff.
+            All my musings on tech tools, developer experience, and one or two
+            wacky stories.
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://linkedin.com/in/dpkreativ"
+        <Link
+          href="/contact"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={inter.className}>
-            LinkedIn <span>-&gt;</span>
+            Contact me <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>
-            Professional posts about software development.
-          </p>
-        </a>
+          <p className={inter.className}>Let's get schwifty!</p>
+        </Link>
       </div>
     </main>
   );
