1d14e62071fe8066e8c1dc717e9884bfa45e64bd
Divine Orji <dpkreativ@gmail.com>
2024-09-04 18:26:33 +0100

    update: I made it responsive for web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="grid gap-20">
+    <main className="grid gap-40 md:grid-cols-3 max-w-6xl mx-auto">
       {/* Hero section */}
-      <section className="w-full max-w-6xl mx-auto p-4 grid gap-8">
+      <section className="p-4 grid gap-8">
         <h1 className="font-serif text-4xl">
           Hi! I'm <span className="italic">Divine</span>.
         </h1>
@@ -24,59 +24,63 @@ export default function Home() {
         <Socials />
       </section>
 
-      {/* About section */}
-      <section className="w-full max-w-6xl mx-auto p-4 grid gap-8">
-        <h2 className="font-serif text-4xl">
-          About <span className="italic">Me</span>
-        </h2>
+      <section className="md:col-span-2">
+        {/* About section */}
+        <section className="p-4 grid gap-8">
+          <h2 className="font-serif text-4xl">
+            About <span className="italic">Me</span>
+          </h2>
 
-        <div className="grid gap-2">
-          <p>
-            I'm an experienced web developer passionate about building dynamic,
-            responsive websites that deliver an exceptional user experience.
-          </p>
-          <p>
-            With a background in design and skills in HTML, CSS, and JavaScript,
-            I turn ideas into reality using the right tools and frameworks.{" "}
-            <Link href="/work" className="font-bold underline">
-              Check out my work
-            </Link>{" "}
-            to see how I do it.
-          </p>
+          <div className="grid gap-2">
+            <p>
+              I'm an experienced web developer passionate about building
+              dynamic, responsive websites that deliver an exceptional user
+              experience.
+            </p>
+            <p>
+              With a background in design and skills in HTML, CSS, and
+              JavaScript, I turn ideas into reality using the right tools and
+              frameworks.{" "}
+              <Link href="/work" className="font-bold underline">
+                Check out my work
+              </Link>{" "}
+              to see how I do it.
+            </p>
 
-          <p>
-            I am active in various developer communities, where I collaborate
-            with peers and contribute to open source projects. I also enjoy
-            breaking down complex ideas on my blogs at{" "}
-            <a
-              href="https://dpkreativ.hashnode.dev"
-              target="_blank"
-              rel="noreferrer"
-              className="font-bold underline"
-            >
-              Hashnode
-            </a>
-            ,{" "}
-            <a
-              href="https://dev.to/dpkreativ"
-              target="_blank"
-              rel="noreferrer"
-              className="font-bold underline"
-            >
-              Dev.to
-            </a>
-            , and{" "}
-            <a
-              href="https://medium.com/@dpkreativ"
-              target="_blank"
-              rel="noreferrer"
-              className="font-bold underline"
-            >
-              Medium
-            </a>
-            .
-          </p>
-        </div>
+            <p>
+              I am active in various developer communities, where I collaborate
+              with peers and contribute to open source projects. I also enjoy
+              breaking down complex ideas on my blogs at{" "}
+              <a
+                href="https://dpkreativ.hashnode.dev"
+                target="_blank"
+                rel="noreferrer"
+                className="font-bold underline"
+              >
+                Hashnode
+              </a>
+              ,{" "}
+              <a
+                href="https://dev.to/dpkreativ"
+                target="_blank"
+                rel="noreferrer"
+                className="font-bold underline"
+              >
+                Dev.to
+              </a>
+              , and{" "}
+              <a
+                href="https://medium.com/@dpkreativ"
+                target="_blank"
+                rel="noreferrer"
+                className="font-bold underline"
+              >
+                Medium
+              </a>
+              .
+            </p>
+          </div>
+        </section>
       </section>
     </main>
   );
