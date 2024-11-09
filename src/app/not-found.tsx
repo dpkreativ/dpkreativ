1615d73cfd07440a86b6e6410b7c3ebe95f24925
Divine Orji <dpkreativ@gmail.com>
2024-11-09 14:10:54 +0100

    hotfixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <main className="flex flex-col items-center justify-center h-screen p-5">
-      <h2>We're still working on this page</h2>
+      <h2>We&apos;re still working on this page</h2>
       {/* <p>Could not find requested resource</p> */}
       <Link href="/">Return Home</Link>
     </main>
